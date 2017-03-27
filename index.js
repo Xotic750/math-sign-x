c655adcbac333f0ce3abb733b0a0d61d056cd0e7
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-27 10:13:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,84 @@
+/**
+ * @file
+ * <a href="https://travis-ci.org/Xotic750/math-sign-x"
+ * title="Travis status">
+ * <img
+ * src="https://travis-ci.org/Xotic750/math-sign-x.svg?branch=master"
+ * alt="Travis status" height="18">
+ * </a>
+ * <a href="https://david-dm.org/Xotic750/math-sign-x"
+ * title="Dependency status">
+ * <img src="https://david-dm.org/Xotic750/math-sign-x.svg"
+ * alt="Dependency status" height="18"/>
+ * </a>
+ * <a
+ * href="https://david-dm.org/Xotic750/math-sign-x#info=devDependencies"
+ * title="devDependency status">
+ * <img src="https://david-dm.org/Xotic750/math-sign-x/dev-status.svg"
+ * alt="devDependency status" height="18"/>
+ * </a>
+ * <a href="https://badge.fury.io/js/math-sign-x" title="npm version">
+ * <img src="https://badge.fury.io/js/math-sign-x.svg"
+ * alt="npm version" height="18">
+ * </a>
+ *
+ * ES6-compliant shim for Math.sign.
+ *
+ * Requires ES3 or above.
+ *
+ * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-math.sign|20.2.2.29 Math.sign(x)}
+ *
+ * @version 1.0.0
+ * @author Xotic750 <Xotic750@gmail.com>
+ * @copyright  Xotic750
+ * @license {@link <https://opensource.org/licenses/MIT> MIT}
+ * @module math-sign-x
+ */
+
+/* eslint strict: 1 */
+
+/* global module */
+
+;(function () { // eslint-disable-line no-extra-semi
+
+  'use strict';
+
+  var $isNaN = require('is-nan');
+
+  var $sign;
+  if (typeof Math.sign === 'function') {
+    try {
+      if (Math.sign(10) === 1 && Math.sign(-10) === -1 && Math.sign(0) === 0) {
+        $sign = Math.sign;
+      }
+    } catch (ignore) {}
+  }
+
+  /**
+   * This method returns the sign of a number, indicating whether the number is positive,
+   * negative or zero.
+   *
+   * @param {*} x A number.
+   * @return {number} A number representing the sign of the given argument. If the argument
+   * is a positive number, negative number, positive zero or negative zero, the function will
+   * return 1, -1, 0 or -0 respectively. Otherwise, NaN is returned.
+   * @example
+   * var mathSign = require('math-sign-x');
+   *
+   * mathSign(3);     //  1
+   * mathSign(-3);    // -1
+   * mathSign('-3');  // -1
+   * mathSign(0);     //  0
+   * mathSign(-0);    // -0
+   * mathSign(NaN);   // NaN
+   * mathSign('foo'); // NaN
+   * mathSign();      // NaN
+   */
+  module.exports = $sign || function sign(x) {
+    var n = Number(x);
+    if (n === 0 || $isNaN(n)) {
+      return n;
+    }
+    return n > 0 ? 1 : -1;
+  };
+}());
