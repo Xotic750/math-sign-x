3ceed336d7bff4db4023190ea15bf6057276fedd
Xotic750 <xotic750@gmail.com>
2017-07-02 15:20:05 +0200

    Update boilerplate and dependecies

--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,81 +1,73 @@
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1, no-magic-numbers: 1, id-length: 1 */
+'use strict';
 
-/* global JSON:true, expect, module, require, describe, it, xit, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var mathSign;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    mathSign = require('../../index.js');
-  } else {
-    mathSign = returnExports;
+var mathSign;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  var ifFunctionsHaveNamesIt = function foo() {}.name === 'foo' ? it : xit;
-
-  var isPositiveZero = function (zero) {
-    return zero === 0 && 1 / zero === Infinity;
-  };
-
-  var isNegativeZero = function (zero) {
-    return zero === 0 && 1 / zero === -Infinity;
-  };
-
-  var numberIsNaN = function (value) {
-    return value !== value;
-  };
-
-  describe('mathSign', function () {
-    ifFunctionsHaveNamesIt('has the right name', function () {
-      expect(mathSign.name).toBe('sign');
-    });
-
-    it('has the right arity', function () {
-      expect(mathSign.length).toBe(1);
-    });
-
-    it('should be correct', function () {
-      // we also verify that [[ToNumber]] is being called
-      [Infinity, 1].forEach(function (value) {
-        expect(mathSign(value)).toBe(1);
-        expect(mathSign(String(value))).toBe(1);
-      });
-      expect(mathSign(true)).toBe(1);
-
-      [-Infinity, -1].forEach(function (value) {
-        expect(mathSign(value)).toBe(-1);
-        expect(mathSign(String(value))).toBe(-1);
-      });
-
-      expect(isPositiveZero(mathSign(+0))).toBe(true);
-      expect(isPositiveZero(mathSign('0'))).toBe(true);
-      expect(isPositiveZero(mathSign('+0'))).toBe(true);
-      expect(isPositiveZero(mathSign(''))).toBe(true);
-      expect(isPositiveZero(mathSign(' '))).toBe(true);
-      expect(isPositiveZero(mathSign(null))).toBe(true);
-      expect(isPositiveZero(mathSign(false))).toBe(true);
-      expect(isNegativeZero(mathSign(-0))).toBe(true);
-      expect(isNegativeZero(mathSign('-0'))).toBe(true);
-      expect(numberIsNaN(mathSign(NaN))).toBe(true);
-      expect(numberIsNaN(mathSign('NaN'))).toBe(true);
-      expect(numberIsNaN(mathSign(undefined))).toBe(true);
-    });
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
+    }
   });
-}());
+  mathSign = require('../../index.js');
+} else {
+  mathSign = returnExports;
+}
+
+var ifFunctionsHaveNamesIt = function foo() {}.name === 'foo' ? it : xit;
+
+var isPositiveZero = function (zero) {
+  return zero === 0 && 1 / zero === Infinity;
+};
+
+var isNegativeZero = function (zero) {
+  return zero === 0 && 1 / zero === -Infinity;
+};
+
+var numberIsNaN = function (value) {
+  return value !== value;
+};
+
+describe('mathSign', function () {
+  ifFunctionsHaveNamesIt('has the right name', function () {
+    expect(mathSign.name).toBe('sign');
+  });
+
+  it('has the right arity', function () {
+    expect(mathSign.length).toBe(1);
+  });
+
+  it('should be correct', function () {
+    // we also verify that [[ToNumber]] is being called
+    [Infinity, 1].forEach(function (value) {
+      expect(mathSign(value)).toBe(1);
+      expect(mathSign(String(value))).toBe(1);
+    });
+    expect(mathSign(true)).toBe(1);
+
+    [-Infinity, -1].forEach(function (value) {
+      expect(mathSign(value)).toBe(-1);
+      expect(mathSign(String(value))).toBe(-1);
+    });
+
+    expect(isPositiveZero(mathSign(+0))).toBe(true);
+    expect(isPositiveZero(mathSign('0'))).toBe(true);
+    expect(isPositiveZero(mathSign('+0'))).toBe(true);
+    expect(isPositiveZero(mathSign(''))).toBe(true);
+    expect(isPositiveZero(mathSign(' '))).toBe(true);
+    expect(isPositiveZero(mathSign(null))).toBe(true);
+    expect(isPositiveZero(mathSign(false))).toBe(true);
+    expect(isNegativeZero(mathSign(-0))).toBe(true);
+    expect(isNegativeZero(mathSign('-0'))).toBe(true);
+    expect(numberIsNaN(mathSign(NaN))).toBe(true);
+    expect(numberIsNaN(mathSign('NaN'))).toBe(true);
+    expect(numberIsNaN(mathSign(undefined))).toBe(true);
+  });
+});
