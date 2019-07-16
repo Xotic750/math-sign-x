59b0a6f4edf605ffaa4b485e4869a242e406cfd9
Graham Fairweather <xotic750@gmail.com>
2019-07-16 23:13:52 +0200

    :bookmark: v4.0.11
    Auto build

--- dist/math-sign-x.js
+++ dist/math-sign-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-16T12:19:26.547Z",
+  "date": "2019-07-16T21:13:46.486Z",
   "describe": "",
   "description": "Shim for Math.sign.",
   "file": "math-sign-x.js",
   "hash": "808ba47444de3a411ba4",
   "license": "MIT",
-  "version": "4.0.10"
+  "version": "4.0.11"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
