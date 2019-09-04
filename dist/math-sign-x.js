2a4d9d634de045677e4dbb46fdccdacb6ca86dfa
Graham Fairweather <xotic750@gmail.com>
2019-09-05 00:59:30 +0200

    Maintenance

--- dist/math-sign-x.js
+++ dist/math-sign-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-08-30T21:46:24.553Z",
+  "date": "2019-09-04T22:59:23.337Z",
   "describe": "",
   "description": "Shim for Math.sign.",
   "file": "math-sign-x.js",
